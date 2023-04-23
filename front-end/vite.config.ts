1b1963e51cabc4a25d0971eca269af05b843e95c
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-04-23 18:05:14 -0300

    feat: implement docker-compose and fix some links

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -15,12 +15,4 @@ export default defineConfig({
       ),
     }),
   ],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 5173,
-  },
 });
